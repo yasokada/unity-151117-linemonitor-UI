b14c1534e1abcf2efde3374ebcd0accc75f649bf
y_okada <yasokada@gmail.com>
2015-11-23 07:12:54 +0900

    SettingSend: add UDP_init()

--- Assets/SettingSend.cs
+++ Assets/SettingSend.cs
@@ -2,6 +2,12 @@
 using System.Collections;
 using UnityEngine.UI;
 
+// for UDP send
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
 public class SettingSend : MonoBehaviour {
 	
 	public const string kCommandIpAddressPrefix = "192.168.10.";
@@ -12,14 +18,31 @@ public class SettingSend : MonoBehaviour {
 	public InputField IF_port;
 	public InputField IF_combaud;
 
+	public bool s_udp_isNotInit = true; // TODO: 1> rename to positive variable
+	UdpClient s_udp_client; 
+
 	void Start () {
 		IF_ipadr.text = kCommandIpAddressPrefix;
 		IF_port.text = kDefaultCommandPort;
 		IF_combaud.text = kDefaultComBaud;
 	}
-	
+
+	void UDP_init() {
+		s_udp_client = new UdpClient ();
+		s_udp_client.Client.ReceiveTimeout = 300; // msec
+		s_udp_client.Client.Blocking = false;
+	}
+
 	public void SendButtonClick() {
 		Debug.Log ("SendButton");
+
+		if (s_udp_isNotInit == true) {
+			s_udp_isNotInit = false;
+			UDP_init ();
+		}
 	}
 }
+
+// byte[] data = System.Text.Encoding.ASCII.GetBytes(text);
+
 
