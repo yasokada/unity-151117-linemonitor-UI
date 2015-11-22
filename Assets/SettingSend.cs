1a3b5ebb3d032dc8d11c650adc860bc149297bc6
y_okada <yasokada@gmail.com>
2015-11-23 06:55:39 +0900

    add SettingSend.cs

---
+++ Assets/SettingSend.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingSend : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SendButtonClick() {
+		Debug.Log ("SendButton");
+	}
+}
+
+// TODO: 1> remove Debug.Log() True/False defined somewhere
