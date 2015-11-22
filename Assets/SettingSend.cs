4ef344ecc3e365fa0f6c33509689b6a0b804af91
y_okada <yasokada@gmail.com>
2015-11-23 07:03:32 +0900

    SettingSend: add default setting > 192.168.10. / 7000 / 9600

--- Assets/SettingSend.cs
+++ Assets/SettingSend.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SettingSend : MonoBehaviour {
+	
+	public const string kCommandIpAddressPrefix = "192.168.10.";
+	public const string kDefaultCommandPort = "7000";
+	public const string kDefaultComBaud = "9600";
 
-	// Use this for initialization
+	public InputField IF_ipadr;
+	public InputField IF_port;
+	public InputField IF_combaud;
+
 	void Start () {
-	
+		IF_ipadr.text = kCommandIpAddressPrefix;
+		IF_port.text = kDefaultCommandPort;
+		IF_combaud.text = kDefaultComBaud;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	public void SendButtonClick() {
 		Debug.Log ("SendButton");
 	}
